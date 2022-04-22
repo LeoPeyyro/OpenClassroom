13183f745fed213c68aa82996f69efb6ae859d0d
LeoPeyyro <leo.perreaut@laposte.net>
2022-04-22 13:34:57 +0200

    administrateur et groupes modifiés à partir de l'admin

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+
+    def __str__(self):
+        return f'{self.name}'
     
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
