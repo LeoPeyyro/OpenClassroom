e9921718da0c94bf4dad528996583df9966232bb
LeoPeyyro <leo.perreaut@laposte.net>
2022-05-02 11:19:52 +0200

    Retrogradation de la version de django

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 from listings.models import Band
+from listings.models import Listing
 
 class BandAdmin(admin.ModelAdmin):
 	list_display = ('name', 'year_formed', 'genre')
 
+class ListingAdmin(admin.ModelAdmin):
+	list_display = ('title', 'year', 'type')
+
 admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
 
 # Register your models here.
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -10,20 +10,24 @@ class Band(models.Model):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
-    
-    class Type(models.TextChoices):
-        RECORDS = 'R'
-        CLOTHING = 'C'
-        POSTERS = 'P'
-        MISCELLANEOUS = 'M'
-    
-    
+         
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
+
+
+class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHING = 'C'
+        POSTERS = 'P'
+        MISCELLANEOUS = 'M'
+
+    title = models.fields.CharField(max_length=100)
     description = models.fields.CharField(blank=True, max_length=1000)
     sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField(null=True, blank=True, validators=[MinValueValidator(1900), MaxValueValidator(2021)])
