14940d70b7f8cb1bc122ecfa0a0c5a2b00cda759
LeoPeyyro <leo.perreaut@laposte.net>
2022-05-06 11:34:25 +0200

    Ajout des Articles

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
+from listings.models import Listing
 
 def band_list(request):
 	bands = Band.objects.all()
@@ -16,3 +17,15 @@ def band_detail(request, id):
 	return render(request,
 		'listings/band_detail.html',
 		{'band': band})
+
+def listing_list(request):
+	listings = Listing.objects.all()
+	return render(request,
+		'listings/listing_list.html',
+		{'listings': listings})
+
+def listing_detail(request, id):
+	listing = Listing.objects.get(id=id)
+	return render(request,
+		'listings/listing_detail.html',
+		{'listing': listing})
