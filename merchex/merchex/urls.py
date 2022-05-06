053f88be9260e850f00407cfabae7b8132685d60
LeoPeyyro <leo.perreaut@laposte.net>
2022-05-06 17:41:43 +0200

    Ajout dans la navigation

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('listings/', views.listing_list, name='listing-list'),
     path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
-    path('about-us/', views.about),
+    path('about-us/', views.about, name='about-us'),
     path('contact-us/', views.contact, name='contact'),
     path('email-sent/', views.email_sent, name='email-sent')
 ]
