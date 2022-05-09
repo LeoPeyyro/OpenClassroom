bf667d169d921c0b5734d86546701fc0ac708e94
LeoPeyyro <leo.perreaut@laposte.net>
2022-05-09 11:59:27 +0200

    Page d'ajout de groupe fonctionnelle

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -22,7 +22,13 @@ def band_detail(request, id):
 		{'band': band})
 
 def band_create(request):
-	form = BandForm()
+	if request.method == 'POST':
+		form = BandForm(request.POST)
+		if form.is_valid():
+			band = form.save()
+			return redirect('band-detail', band.id)
+	else:
+		form = BandForm()
 	return render(request,
 		'listings/band_create.html',
 		{'form': form})
